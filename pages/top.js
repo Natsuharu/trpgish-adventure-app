6d327d33e4208ed2f55defe84e119373ff2dbe86
Natsuharu <s3312985@gmail.com>
2023-04-30 17:41:45 +0900

    <fix>戦闘中のテキスト表示を修正

--- pages/top.js
+++ pages/top.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector(".start-btn");
 const statusBtn = document.querySelector(".status-btn");
 const healthValue = document.querySelector(".health");
+const valueOut = document.querySelector(".out");
 
 window.addEventListener(
   "animationend",
@@ -57,20 +58,25 @@ window.onkeydown = function (el) {
 class NameAnimation {
   constructor(el) {
     let key = el.key;
-
-    if (el.keyCode === 8) {
-      keyString = keyString.slice(0, -1);
-      document.querySelector(".out").innerHTML = keyString;
-    } else if (el.keyCode === 0) {
+    if (el.keyCode === 0) {
+      //変換かな
       event.preventDefault();
+    } else if (el.keyCode === 8) {
+      //back
+      keyString = keyString.slice(0, -1);
+      valueOut.innerHTML = keyString;
+    } else if (el.keyCode === 89 && settings.gameStart.Boolean === true) {
+      // y
+      const bs = new BattleScene();
+      bs.Slime();
     } else {
       keyString += key;
-      document.querySelector(".out").innerHTML = keyString;
+      valueOut.innerHTML = keyString;
     }
   }
 }
 
-//ステータス割り振り
+//ステータス
 const statuses = {
   str: {
     min: 3,
@@ -106,63 +112,94 @@ const statuses = {
   },
 };
 
+//テキスト文
 const texts = {
   encounter: {
     text: "モンスターがあらわれた！",
   },
-  battle: {
-    text: "こうげきしますか？ Y or N (キーを押してください)",
+  attack: {
+    text: "こうげきしますか？ Y (キーを押すとこうげきします)",
+  },
+  heroAttack: {
+    text: "のこうげき！",
   },
   magic: {
-    text: "まほうをつかいますか？ Y or N (キーを押してください)",
+    text: "まほうをつかいますか？ Y (キーを押すとこうげきします)",
+  },
+  enemyAttack: {
+    text: "敵のこうげき！",
   },
 };
 
-let flag = false;
+const settings = {
+  gameStart: {
+    Boolean: false,
+  },
+  flag: {
+    Boolean: false,
+  },
+  action: {
+    choice: null,
+  },
+};
+
+//モンスター
+const monsters = {
+  slime: {
+    hp: 10,
+    min: 1,
+    max: 5,
+    dex: 3,
+  },
+};
+
 let health = "";
 statusBtn.addEventListener("click", function () {
-  flag = true;
+  settings.flag.Boolean = true;
+
   const rn = new RandomNumber();
+
+  const keys = Object.keys(statuses);
+  keys.forEach((status) => {
+    let type = status;
+    let minNum = statuses[type].min;
+    let maxNum = statuses[type].max;
+
+    let num = rn.number(minNum, maxNum);
+    document.querySelector(`.${type}`).innerHTML = num;
+    document.querySelector(`.${type}`).style.padding = "8.5px";
+    statuses[type].valueOfStatus = num;
+  });
+  healthValue.innerHTML = rn.health(
+    statuses.con.valueOfStatus,
+    statuses.siz.valueOfStatus
+  );
+
   const cg = new ChangeHtml();
   cg.change(".mental", statuses.pow.valueOfStatus);
 });
 
+//ダイスロール
 class RandomNumber {
-  constructor() {
-    const keys = Object.keys(statuses);
-    keys.forEach((status) => {
-      let type = status;
-      let minNum = statuses[type].min;
-      let maxNum = statuses[type].max;
-
-      let num = this._Number(minNum, maxNum);
-      document.querySelector(`.${type}`).innerHTML = num;
-      document.querySelector(`.${type}`).style.padding = "8.5px";
-      statuses[type].valueOfStatus = num;
-    });
-    healthValue.innerHTML = this._Health(
-      statuses.con.valueOfStatus,
-      statuses.siz.valueOfStatus
-    );
-  }
-
-  _Number(min, max) {
+  number(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
   }
-  _Health(con, siz) {
+  health(con, siz) {
     return Math.floor((con + siz) / 2);
   }
 }
 
 //ゲームスタート
+let hero_name = "";
 startBtn.addEventListener("click", function () {
-  statuses.valueOfHeroName = document.querySelector(".out").innerHTML;
+  settings.gameStart.Boolean = true;
+  statuses.valueOfHeroName = valueOut.innerHTML;
 
-  const hero_name = statuses.valueOfHeroName;
+  hero_name = statuses.valueOfHeroName;
 
   if (hero_name === "") {
     alert("名前を入力してください");
-  } else if (flag === false) {
+  } else if (settings.flag.Boolean === false) {
     alert("ステータスを振ってください");
   } else {
     const hc = new HideClass();
@@ -194,6 +231,13 @@ class ChangeHtml {
   change(el, contain) {
     document.querySelector(el).innerHTML = contain;
   }
+  heroChange(el, contain) {
+    const rn = new RandomNumber();
+    let damage = rn.number(monsters.slime.min, monsters.slime.max);
+
+    document.querySelector(el).innerHTML =
+      hero_name + " " + contain + ` 敵は ${damage} のダメージをうけた！`;
+  }
 }
 
 class Transform {
@@ -220,6 +264,7 @@ class Choice {
     const actionChoice = document.querySelectorAll(`.${el}`);
     let target = elem.target.className;
     let targetId = elem.target.id;
+
     if (target === el) {
       actionChoice.forEach(function (e) {
         let temp = e.querySelector(".choice");
@@ -233,11 +278,27 @@ class Choice {
       elem.target.appendChild(choice);
 
       const cg = new ChangeHtml();
-      if (targetId === "battle") {
-        cg.change(".text", texts.battle.text);
+      if (targetId === "attack") {
+        cg.change(".text", texts.attack.text);
+        settings.action.choice = "attack";
       } else if (targetId === "magic") {
         cg.change(".text", texts.magic.text);
+        settings.action.choice = "magic";
       }
     }
+  }
+}
+
+class BattleScene {
+  Slime() {
+    const cg = new ChangeHtml();
+    if (monsters.slime.dex > statuses.dex.valueOfStatus) {
+      cg.change(".text", texts.enemyAttack.text);
+    } else {
+      cg.heroChange(".text", texts.heroAttack.text);
+    }
+    // if (settings.action.choice === "attack") {
+    //   console.log("yes");
+    // }
   }
 }
