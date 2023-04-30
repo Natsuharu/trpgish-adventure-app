b321b79579f9cb850a9093ab21a49168d00686c3
Natsuharu <>
2023-04-30 09:59:31 +0900

    <fix>テキスト表示、ステータス書き換え部分の修正

--- pages/top.js
+++ pages/top.js
@@ -1,38 +1,12 @@
 const startBtn = document.querySelector(".start-btn");
 const statusBtn = document.querySelector(".status-btn");
-
-document.addEventListener(
-  "click",
-  (elem) => {
-    const ch = new Choice();
-    ch.action("action", elem);
-  },
-  false
-);
-
-class Choice {
-  action(el, elem) {
-    const actionChoice = document.querySelectorAll(`.${el}`);
-    actionChoice.forEach(function (e) {
-      let temp = e.querySelector(".choice");
-      if (temp !== null) {
-        e.removeChild(temp);
-      }
-    });
-
-    let target = elem.target.className;
-    if (target === el) {
-      const choice = document.createElement("span");
-      choice.className = "choice";
-      elem.target.appendChild(choice);
-    }
-  }
-}
+const healthValue = document.querySelector(".health");
 
 window.addEventListener(
   "animationend",
   function () {
-    const ic = new InviewClass(".main-container");
+    const ic = new InviewClass();
+    ic.inview(".main-container");
     const hc = new HideClass();
     hc.hide(".container");
   },
@@ -40,7 +14,7 @@ window.addEventListener(
 );
 
 class InviewClass {
-  constructor(el) {
+  inview(el) {
     this.el = document.querySelectorAll(el);
     this.el.forEach(function (e) {
       e.classList.add("inview");
@@ -96,70 +70,95 @@ class NameAnimation {
   }
 }
 
+//ステータス割り振り
+const statuses = {
+  str: {
+    min: 3,
+    max: 18,
+  },
+  con: {
+    min: 3,
+    max: 18,
+  },
+  dex: {
+    min: 3,
+    max: 18,
+  },
+  app: {
+    min: 3,
+    max: 18,
+  },
+  pow: {
+    min: 3,
+    max: 18,
+  },
+  siz: {
+    min: 8,
+    max: 18,
+  },
+  int: {
+    min: 8,
+    max: 18,
+  },
+  edu: {
+    min: 6,
+    max: 21,
+  },
+};
+
+const texts = {
+  encounter: {
+    text: "モンスターがあらわれた！",
+  },
+  battle: {
+    text: "こうげきしますか？ Y or N (キーを押してください)",
+  },
+  magic: {
+    text: "まほうをつかいますか？ Y or N (キーを押してください)",
+  },
+};
+
 let flag = false;
+let health = "";
 statusBtn.addEventListener("click", function () {
   flag = true;
   const rn = new RandomNumber();
+  const cg = new ChangeHtml();
+  cg.change(".mental", statuses.pow.valueOfStatus);
 });
 
 class RandomNumber {
   constructor() {
-    const todos = {
-      str: {
-        min: 3,
-        max: 18,
-      },
-      con: {
-        min: 3,
-        max: 18,
-      },
-      dex: {
-        min: 3,
-        max: 18,
-      },
-      app: {
-        min: 3,
-        max: 18,
-      },
-      pow: {
-        min: 3,
-        max: 18,
-      },
-      siz: {
-        min: 8,
-        max: 18,
-      },
-      int: {
-        min: 8,
-        max: 18,
-      },
-      edu: {
-        min: 6,
-        max: 21,
-      },
-    };
-
-    const keys = Object.keys(todos);
-    keys.forEach((todo) => {
-      let type = todo;
-      let minNum = todos[type].min;
-      let maxNum = todos[type].max;
+    const keys = Object.keys(statuses);
+    keys.forEach((status) => {
+      let type = status;
+      let minNum = statuses[type].min;
+      let maxNum = statuses[type].max;
 
       let num = this._Number(minNum, maxNum);
-      document.querySelector(`.${todo}`).innerHTML = num;
-      document.querySelector(`.${todo}`).style.padding = "8.5px";
+      document.querySelector(`.${type}`).innerHTML = num;
+      document.querySelector(`.${type}`).style.padding = "8.5px";
+      statuses[type].valueOfStatus = num;
     });
+    healthValue.innerHTML = this._Health(
+      statuses.con.valueOfStatus,
+      statuses.siz.valueOfStatus
+    );
   }
 
   _Number(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
   }
+  _Health(con, siz) {
+    return Math.floor((con + siz) / 2);
+  }
 }
 
 //ゲームスタート
 startBtn.addEventListener("click", function () {
-  const hn = new HeroName();
-  const hero_name = hn.name();
+  statuses.valueOfHeroName = document.querySelector(".out").innerHTML;
+
+  const hero_name = statuses.valueOfHeroName;
 
   if (hero_name === "") {
     alert("名前を入力してください");
@@ -172,22 +171,28 @@ startBtn.addEventListener("click", function () {
 
     const tf = new Transform();
     tf.transform(".text-box");
+    tf.transform(".text");
     tf.transform(".status-container");
     tf.transform(".character-img");
 
-    document.querySelector(".text").innerHTML = "モンスターがあらわれた！";
+    const cg = new ChangeHtml();
+    cg.change(".character-name", hero_name);
+    cg.change(".text", texts.encounter.text);
+
     const ta = new TextAnimation(".text");
 
     tf.transform(".back-img");
     tf.transform(".back-img2");
 
-    const ic = new InviewClass(".character-img2");
+    const ic = new InviewClass();
+    ic.inview(".character-img2");
+    ic.inview(".action-choice");
   }
 });
 
-class HeroName {
-  name() {
-    return document.querySelector(".out").innerHTML;
+class ChangeHtml {
+  change(el, contain) {
+    document.querySelector(el).innerHTML = contain;
   }
 }
 
@@ -197,5 +202,42 @@ class Transform {
     this.el.forEach(function (e) {
       e.classList.add("transform");
     });
+  }
+}
+
+//アクション選択
+document.addEventListener(
+  "click",
+  (elem) => {
+    const ch = new Choice();
+    ch.action("action", elem);
+  },
+  false
+);
+
+class Choice {
+  action(el, elem) {
+    const actionChoice = document.querySelectorAll(`.${el}`);
+    let target = elem.target.className;
+    let targetId = elem.target.id;
+    if (target === el) {
+      actionChoice.forEach(function (e) {
+        let temp = e.querySelector(".choice");
+        if (temp !== null) {
+          e.removeChild(temp);
+        }
+      });
+
+      const choice = document.createElement("span");
+      choice.className = "choice";
+      elem.target.appendChild(choice);
+
+      const cg = new ChangeHtml();
+      if (targetId === "battle") {
+        cg.change(".text", texts.battle.text);
+      } else if (targetId === "magic") {
+        cg.change(".text", texts.magic.text);
+      }
+    }
   }
 }
